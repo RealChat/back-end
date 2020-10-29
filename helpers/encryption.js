10d1052e841982387ae3759dcfb2b6974addab75
Ashutosh Singhai <ashutosh.singhai@inviz.ai>
2020-10-29 08:35:34 +0530

    replaced line 3 with 4 in encryption.js

--- helpers/encryption.js
+++ helpers/encryption.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
-const salt = crypto.randomBytes(16).toString("hex");
+// const salt = crypto.randomBytes(16).toString("hex");
+const salt = "3689387f7246b15dbd107c1edcbe6cb2";
 
 function encrypt(password) {
     return new Promise((resolve, reject) => {
